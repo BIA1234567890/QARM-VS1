365d6fc9f3fe670d8d445e54738baa5871b5e237
BIA1234567890 <127410779+BIA1234567890@users.noreply.github.com>
2025-11-28 13:29:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ def page_about():
     col6, col7 = st.columns([1, 1.5])
     with col6:
         st.markdown('<div class="image-frame">', unsafe_allow_html=True)
-        st.image("team.png", use_column_width=True)
+        st.image("Team.png", use_column_width=True)
         st.markdown('</div>', unsafe_allow_html=True)
     with col7:
         st.markdown('<div class="section-heading"> Who We Serve</div>', unsafe_allow_html=True)
